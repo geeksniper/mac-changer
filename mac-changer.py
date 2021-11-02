be3b8d8cb926e9c89bc4774040c84bff7de4581c
dev-gsniper <71917246+geeksniper@users.noreply.github.com>
2021-11-02 16:15:33 +0600

    Update mac-changer.py

--- mac-changer.py
+++ mac-changer.py
@@ -7,12 +7,16 @@ parser.add_option("-m", dest="new_mac", help="new mac address")
 
 (options, arguments) = parser.parse_args()
 
+if not options.interface:
+    parser.error("please specify interface, use --help for more information")
+elif not options.new_mac:
+    parser.error("please specify mac-address, use --help for more information")
+
 def func(interface, new_mac):
 
     subprocess.call(["sudo", "ifconfig", interface, "down"])
     subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["sudo", "ifconfig", interface, "up"])
     print("your current mac is: " + new_mac)
-interface = options.interface
-new_mac = options.new_mac
-func(interface, new_mac)
+    
+func(options.interface, options.new_mac)
